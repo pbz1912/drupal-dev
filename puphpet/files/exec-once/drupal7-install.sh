10e9824b87d8b2e72e12cd85a8ac677978f67043
Alex Skrypnyk <alex.skrypnyk@eurekareport.com.au>
2014-03-29 21:12:18 +1100

    Added Drupal 7 command line installation script to tun once on the very frst vagrant up.

---
+++ puphpet/files/exec-once/drupal7-install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Installs Drupal 7 using drush.
+
+#
+# Variables below correspond to values in PuPHPet's config.yml.
+#
+
+# Drupal install directory.
+DRUPAL_DIR=/www/drupal7
+
+# Drupal URL.
+DRUPAL_URI=drupal7.local
+
+# MySQL DB name and credentials.
+DRUPAL_MYSQL_USER=admin
+DRUPAL_MYSQL_PASSWORD=password
+DRUPAL_MYSQL_DB=drupal7
+
+# Drupal administrator's credentials.
+DRUPAL_USER_NAME=admin
+DRUPAL_USER_PASSWORD=password
+DRUPAL_USER_MAIL=admin@example.com
+
+# Start installation.
+echo '****** DRUPAL 7 INSTALLATION ******'
+drush --root=${DRUPAL_DIR} --uri=http://${DRUPAL_URI} site-install standard --db-url=mysql://${DRUPAL_MYSQL_USER}:${DRUPAL_MYSQL_PASSWORD}@localhost/${DRUPAL_MYSQL_DB} --account-name=${DRUPAL_USER_NAME} --account-pass=${DRUPAL_USER_PASS} --account-mail=${DRUPAL_MAIL} --site-mail=${DRUPAL_MAIL} --site-name='Drupal 7'
+
+# Change permissions for files directory.
+sudo sh -c "chmod g+s ${DRUPAL_DIR}/sites/default/files"
